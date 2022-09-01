a0f9d10f41af898700e6fc9b8de6efa85fcc4273
Prasadcloudmaster <105862856+Prasadcloudmaster@users.noreply.github.com>
2022-09-01 17:18:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-143"
+    bucket = "mydev-tf-state-bucket-14"
     key = "main"
-    region = "us-east-2"
+    region = "eu-west-2"
     dynamodb_table = "my-dynamodb-table"
   }
 }
